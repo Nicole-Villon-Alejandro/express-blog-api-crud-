f7bd90c5ab3a04c97f4722578e05f1cf21f100b5
Nicole-Villon-Alejandro <nicolevillon5@gmail.com>
2025-03-04 16:43:58 +0100

    add_changes

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
 const postRouter = require('./routers/postsRouter');
 
 //middlewares
 const notFound = require('./middlewares/notFound');
 const errorsHandler = require('./middlewares/errorsHandler');
+
+//middlewares cors
+app.use(cors(
+    {
+        origin: 'http://localhost:5173'
+    }
+))
 
 app.use(express.static('public'));
 
